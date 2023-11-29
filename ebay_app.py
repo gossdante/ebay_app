5e9dbddb60100741bab7852476b725694580d53b
Dante Goss II <97195740+gossdante@users.noreply.github.com>
2023-11-29 05:27:36 +0000

    added linkedin

--- ebay_app.py
+++ ebay_app.py
@@ -264,6 +264,7 @@ def gpu_marks():
 
 st.title('Ebay Price Performance')
 st.write("This application will access ebay data for a list of CPUs, GPUs, and Motherboards.")# Note that the first time this web application is loaded, it may take several minutes to search for all the data, though this will be retained on subsequent loads. ")
+st.write("If you find this app useful and would like to add new parts send me a message on LinkedIn! https://www.linkedin.com/in/gossdante/")
 # st.write('To get newly updated data, press the "Rerun" button in the menu bar above.')
 #st.write('Click the buttons below to load recent price data')
 
